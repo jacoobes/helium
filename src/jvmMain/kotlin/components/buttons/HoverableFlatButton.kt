a6a50c6ea905361f9476b6794734c0645c99d17d
DasIschBims <46683337+DasIschBims@users.noreply.github.com>
2023-01-05 23:13:04 +0100

    fixed button width and height (e. g. minimize)

--- src/jvmMain/kotlin/components/buttons/HoverableFlatButton.kt
+++ src/jvmMain/kotlin/components/buttons/HoverableFlatButton.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
+import buttonSizes
 
 //TODO: Fix text overflow
 @Composable
@@ -38,8 +39,7 @@ fun HoverableFlatButton(
         modifier = mod,
     ) {
         //material spec (ish) https://m3.material.io/components/top-app-bar/specs
-        val buttonSizes = (maxHeight * 5) / 8
-        val pad = (maxHeight - buttonSizes) / 2
+        val pad = 12.dp
         val startEnd = PaddingValues(start = pad, end = pad)
         Box(
             contentAlignment = Alignment.Center,
