3d50f036a9637879031b86724247767faa5651eb
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-05-26 00:09:40 -0500

    add studd

--- src/jvmMain/kotlin/components/textarea/TabView.kt
+++ src/jvmMain/kotlin/components/textarea/TabView.kt
@@ -1,9 +1,14 @@
 package components.textarea
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import structs.Editor
 import kotlin.io.path.name
 
@@ -20,7 +25,10 @@ fun TabView(
                    Tab(selectedTabIndex.value == idx, onClick = {
                        selectedTabIndex.value = idx
                    }) {
-                       Text(editor.code.path.name)
+                       Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween){
+                            IconButton({}) { Icon(Icons.Default.Close, "close tab") }
+                           Text(editor.code.path.name)
+                       }
                    }
                 }
             }
